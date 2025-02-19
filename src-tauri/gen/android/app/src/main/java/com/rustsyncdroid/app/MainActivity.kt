fb589a148dbc833eb612f50ca3162e85c87000ea
Christopber Vos <christophervos522@gmail.com>
2025-02-19 11:53:30 +0200

    changed appbar color to black and added buttom navbar

--- src-tauri/gen/android/app/src/main/java/com/rustsyncdroid/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/rustsyncdroid/app/MainActivity.kt
@@ -1,3 +1,17 @@
 package com.rustsyncdroid.app
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : TauriActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    // Allow content to extend under the system bars (if necessary)
+    WindowCompat.setDecorFitsSystemWindows(window, true)
+
+    // Set the status bar color to a normal color (replace with your color)
+    window.statusBarColor = android.graphics.Color.parseColor("#000000")  // Black color example
+  }
+}
--- src/pages/Rootlayout.tsx
+++ src/pages/Rootlayout.tsx
@@ -1,11 +1,59 @@
-import { Outlet } from "react-router-dom";
-import { useSession } from "../store/Session";
+import { Outlet, useNavigate } from "react-router-dom";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Container,
+} from "@mui/material";
+import { useState } from "react";
+
+import { HomeMaxTwoTone, SyncAltTwoTone } from "@mui/icons-material";
 
 function Rootlayout() {
+  const navigate = useNavigate();
+  const [value, setValue] = useState("home");
+
+  const map = {
+    "home": "/",
+    "sync": "/sync",
+  }
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+    navigate(map[newValue] || "/");
+  };
+
+
   return (
-    <>
-      <Outlet />
-    </>
+    <Container
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+      }}
+    >
+      <Box sx={{ width: "100%", height: "94vh", margin: 0 }}>
+        <Outlet />
+      </Box>
+      <BottomNavigation
+        sx={{ height: "6vh" }}
+        value={value}
+        onChange={handleChange}
+      >
+        <BottomNavigationAction
+          label="Home"
+          value="home"
+          icon={<HomeMaxTwoTone />}
+        />
+        <BottomNavigationAction
+          label="Sync"
+          value="sync"
+          icon={<SyncAltTwoTone />}
+        />
+      </BottomNavigation>
+    </Container>
   );
 }
 
